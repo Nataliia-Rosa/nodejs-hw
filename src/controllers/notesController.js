34d649af5e78345ce670d31fe96a2d9452062891
Natalia <rosa.nataliya5@gmail.com>
2026-05-30 22:26:22 +0200

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -8,19 +8,34 @@ export const getAllNotes = async (req, res) => {
   const currentPerPage = Number(perPage);
   const skip = (currentPage - 1) * currentPerPage;
 
-  let notesQuery = Note.find({ userId: req.user._id });
+  const filter = {
+    userId: req.user._id,
+  };
 
   if (tag) {
-    notesQuery = notesQuery.where('tag').equals(tag);
+    filter.tag = tag;
   }
 
   if (search) {
-    notesQuery = notesQuery.find({ $text: { $search: search } });
+    filter.$or = [
+      {
+        title: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+      {
+        content: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+    ];
   }
 
-  const [totalNotes, notes] = await Promise.all([
-    Note.find({ userId: req.user._id }).merge(notesQuery).countDocuments(),
-    notesQuery.skip(skip).limit(currentPerPage),
+  const [notes, totalNotes] = await Promise.all([
+    Note.find(filter).skip(skip).limit(currentPerPage),
+    Note.countDocuments(filter),
   ]);
 
   const totalPages = Math.ceil(totalNotes / currentPerPage);
@@ -58,21 +73,6 @@ export const createNote = async (req, res) => {
   res.status(201).json(note);
 };
 
-export const deleteNote = async (req, res) => {
-  const { noteId } = req.params;
-
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-    userId: req.user._id,
-  });
-
-  if (!note) {
-    throw createHttpError(404, 'Note not found');
-  }
-
-  res.status(200).json(note);
-};
-
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
 
@@ -93,4 +93,19 @@ export const updateNote = async (req, res) => {
   }
 
   res.status(200).json(updatedNote);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
 };
