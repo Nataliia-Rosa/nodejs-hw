50820dddcb43afd819cf93ef3543a16776e7670a
Natalia <rosa.nataliya5@gmail.com>
2026-05-30 22:23:42 +0200

    Update authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,26 +3,24 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { accessToken } = req.cookies;
+const { sessionId, accessToken } = req.cookies;
 
-  // 1. нет токена
-  if (!accessToken) {
-    return next(createHttpError(401, 'Missing access token'));
-  }
+if (!sessionId || !accessToken) {
+  return next(createHttpError(401, 'Not authorized'));
+}
 
-  // 2. ищем сессию
-  const session = await Session.findOne({ accessToken });
+const session = await Session.findOne({
+  _id: sessionId,
+  accessToken,
+});
 
   if (!session) {
     return next(createHttpError(401, 'Session not found'));
   }
-
-  // 3. проверяем срок
   if (new Date() > session.accessTokenValidUntil) {
     return next(createHttpError(401, 'Access token expired'));
   }
 
-  // 4. ищем пользователя
   const user = await User.findById(session.userId);
 
   if (!user) {
