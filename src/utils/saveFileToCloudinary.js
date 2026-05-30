7ee27b5b82f5354dca004996ac79e24e950dbfd8
Natalia <rosa.nataliya5@gmail.com>
2026-05-30 22:46:56 +0200

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -7,11 +7,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const saveFileToCloudinary = (buffer) => {
+export const saveFileToCloudinary = (buffer, userId) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
-        folder: 'avatars',
+        resource_type: 'image',
+        public_id: userId,
+        overwrite: true,
+        unique_filename: false,
       },
       (error, result) => {
         if (error) {
